8879e12a126e874c8f847430c8fd41fbcc4885ea
vladimir.milosevic <vladimirmilosevic1995@gmail.com>
2023-12-23 03:15:07 +0100

    state pharmacy component, spending medicaments component

--- anihis-fe/apps/anihis-portal/src/app/tab-groups/pharmacy/spending-medicaments/spending-medicaments.component.ts
+++ anihis-fe/apps/anihis-portal/src/app/tab-groups/pharmacy/spending-medicaments/spending-medicaments.component.ts
@@ -1,14 +1,82 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBaseComponent } from '../../../shared/base-components/form-base.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'anihis-spending-medicaments',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './spending-medicaments.component.html',
   styleUrls: ['./spending-medicaments.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SpendingMedicamentsComponent {
+export class SpendingMedicamentsComponent
+  extends FormBaseComponent
+  implements AfterViewInit
+{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  // data$ = this.excellentVaccinationsService
+  //   .fetchData()
+  //   .pipe(tap((results) => this.prepareDataSource(results)));
+
+  pharmacies: any = [];
+
+  displayedColumns: string[] = [
+    'medicament',
+    'totalInput',
+    'totalOutput',
+    'state',
+    'priceInput',
+    'priceOutput',
+    'balance',
+  ];
+  dataSource = new MatTableDataSource<any[]>([]);
+
+  pageSize!: number;
+  pageIndex!: number;
+  override form = this.fb.group({
+    date: [''],
+    dateTo: [''],
+    pharmacy: [''],
+  });
+
+  constructor() {
+    // private excellentVaccinationsService: ExcellentVaccinationsService
+    super();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  prepareDataSource(manufacturers: any) {
+    const formattedData = manufacturers.map((manufacturer: any) => ({
+      uid: manufacturer.uid,
+      name: manufacturer.name,
+      city: manufacturer.city,
+      address: manufacturer.address,
+      phoneNumber: manufacturer.phoneNumber,
+      mobileNumber: manufacturer.mobileNumber,
+      bankAccount: manufacturer.bankAccount,
+      contactPerson: manufacturer.contactPerson,
+    }));
+    this.dataSource.data = formattedData;
+  }
+
+  onPageChange(event: any): void {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  saveChanges() {}
 }
--- anihis-fe/apps/anihis-portal/src/app/tab-groups/pharmacy/state-pharmacy/state-pharmacy.component.ts
+++ anihis-fe/apps/anihis-portal/src/app/tab-groups/pharmacy/state-pharmacy/state-pharmacy.component.ts
@@ -1,14 +1,85 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBaseComponent } from '../../../shared/base-components/form-base.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'anihis-state-pharmacy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './state-pharmacy.component.html',
   styleUrls: ['./state-pharmacy.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatePharmacyComponent {
+export class StatePharmacyComponent
+  extends FormBaseComponent
+  implements AfterViewInit
+{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  // data$ = this.excellentVaccinationsService
+  //   .fetchData()
+  //   .pipe(tap((results) => this.prepareDataSource(results)));
+
+  pharmacies: any = [];
+  typeMedicaments: any = [];
+
+  displayedColumns: string[] = [
+    'medicament',
+    'species',
+    'label',
+    'unitMeasure',
+    'sellingPrice',
+    'purchasePrice',
+    'totalInput',
+    'totalOutput',
+    'state',
+    'optimal',
+    'minimum',
+  ];
+  dataSource = new MatTableDataSource<any[]>([]);
+
+  pageSize!: number;
+  pageIndex!: number;
+  override form = this.fb.group({
+    date: [''],
+    dateTo: [''],
+    typeOfVaccine: [''],
+  });
+
+  constructor() {
+    // private excellentVaccinationsService: ExcellentVaccinationsService
+    super();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  prepareDataSource(manufacturers: any) {
+    const formattedData = manufacturers.map((manufacturer: any) => ({
+      uid: manufacturer.uid,
+      name: manufacturer.name,
+      city: manufacturer.city,
+      address: manufacturer.address,
+      phoneNumber: manufacturer.phoneNumber,
+      mobileNumber: manufacturer.mobileNumber,
+      bankAccount: manufacturer.bankAccount,
+      contactPerson: manufacturer.contactPerson,
+    }));
+    this.dataSource.data = formattedData;
+  }
+
+  onPageChange(event: any): void {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.dataSource.paginator = this.paginator;
+  }
 }
